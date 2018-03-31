37e22c9723fbae343709249aa36d155fbedb7d7f
Marlon <martianworm17@gmail.com>
2018-03-31 21:09:26 +0800

    Model-View-Controller: In Practice

--- src/com/kamotelabs/demo1/Application.java
+++ src/com/kamotelabs/demo1/Application.java
@@ -1,5 +1,29 @@
 package com.kamotelabs.demo1;
 
+import javax.swing.SwingUtilities;
+
+import com.kamotelabs.designpatterns.demo1.model.Model;
+import com.kamotelabs.designpatterns.demo1.view.View;
+
 public class Application {
+	
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				runApp();
+			}
+		});
+	}
+	
+	public static void runApp() {
+		Model model = new Model();
+		
+		View view = new View(model);
+		
+		Controller controller = new Controller(model, view);
+	}
 
 }
--- src/com/kamotelabs/designpatterns/demo1/controller/Controller.java
+++ src/com/kamotelabs/designpatterns/demo1/controller/Controller.java
@@ -1,5 +1,18 @@
 package com.kamotelabs.designpatterns.demo1.controller;
 
+import com.kamotelabs.designpatterns.demo1.model.Model;
+import com.kamotelabs.designpatterns.demo1.view.View;
+
 public class Controller {
+	
+	private View view;
+	private Model model;
+	
+	public Controller(View view, Model model) {
+		this.view = view;
+		this.model = model;
+	}
+	
+	
 
 }

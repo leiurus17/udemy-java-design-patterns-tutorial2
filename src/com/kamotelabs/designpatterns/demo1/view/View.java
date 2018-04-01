73cd9c9052ddd7602b0ce76cb0b6ccf8a99b4d25
Marlon <martianworm17@gmail.com>
2018-04-01 14:27:30 +0800

    Observer Pattern: An Example Involving Buttons

--- src/com/kamotelabs/designpatterns/demo1/view/View.java
+++ src/com/kamotelabs/designpatterns/demo1/view/View.java
@@ -2,26 +2,28 @@ package com.kamotelabs.designpatterns.demo1.view;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import com.kamotelabs.designpatterns.demo1.model.Model;
 
-public class View extends JFrame {
-	
+public class View extends JFrame implements ActionListener {
+
 	private Model model;
 	private JButton helloButton;
 
 	public View(Model model) {
 		super("MVC Demo");
-		
+
 		this.model = model;
-		
+
 		helloButton = new JButton("Click Me!");
-		
+
 		setLayout(new GridBagLayout());
-		
+
 		GridBagConstraints gc = new GridBagConstraints();
 		gc.anchor = GridBagConstraints.CENTER;
 		gc.gridheight = 0;
@@ -29,12 +31,19 @@ public class View extends JFrame {
 		gc.weightx = 1;
 		gc.weighty = 1;
 		gc.fill = GridBagConstraints.NONE;
-		
+
 		add(helloButton, gc);
-		
+
+		helloButton.addActionListener(this);
+
 		setSize(600, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("Par!");
 	}
 
 }

9dfd96e13b9c3149676e810b1c0f8c796fe5892e
Marlon <martianworm17@gmail.com>
2018-04-09 23:45:25 +0800

    Adapter Pattern

--- src/com/kamotelabs/com/adapter/SwingApp.java
+++ src/com/kamotelabs/com/adapter/SwingApp.java
@@ -1,7 +1,7 @@
 package com.kamotelabs.com.adapter;
 
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
 
@@ -20,48 +20,11 @@ public class SwingApp {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
-		frame.addWindowListener(new WindowListener() {
-
-			@Override
-			public void windowActivated(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowClosed(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowClosing(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowDeactivated(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowDeiconified(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void windowIconified(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
+		frame.addWindowListener(new WindowAdapter() {
 
 			@Override
 			public void windowOpened(WindowEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				System.out.println("Window opened.");
 			}
 			
 		});

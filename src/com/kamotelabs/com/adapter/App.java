5664d7ef6730d6362bb3f3453a49db5d5fe8b3c4
Marlon <martianworm17@gmail.com>
2018-04-09 23:38:25 +0800

    Adapter Pattern

--- src/com/kamotelabs/com/adapter/App.java
+++ src/com/kamotelabs/com/adapter/App.java
@@ -4,10 +4,17 @@ public class App {
 
 	public static void main(String[] args) {
 
+		// Composition Adapter
 		ConsoleLogWriter logWriter = new ConsoleLogWriter();
+		
+		// Inheritance Adapter
+		ConsoleLogWriter2 logWriter2 = new ConsoleLogWriter2();
+		
 		Logger logger = new Logger(logWriter);
+		Logger logger2 = new Logger(logWriter2);
 		
 		logger.write("Hello there!");
+		logger2.write("The angel from my nightmare.");
 	}
 
 }

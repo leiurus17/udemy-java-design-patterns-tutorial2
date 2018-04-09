2040729bf916ed2df4f92c10ed882afb9a127de4
Marlon <martianworm17@gmail.com>
2018-04-09 23:03:48 +0800

    Facades

--- src/com/kamotelabs/com/facade/App.java
+++ src/com/kamotelabs/com/facade/App.java
@@ -4,19 +4,10 @@ public class App {
 
 	public static void main(String[] args) {
 
-		InputSystem input = new InputSystem();
-		GameObjects objects = new GameObjects();
-		GameConsole screen = new GameConsole();
+		Game game = new Game();
 		
 		while(true) {
-			// Input
-			input.getInput();
-			
-			// Update game objects (player, bad guys, etc)
-			objects.update(input);
-			
-			// Draw
-			screen.draw(objects);
+			game.update();
 		}
 		
 	}
--- src/com/kamotelabs/com/facade/Game.java
+++ src/com/kamotelabs/com/facade/Game.java
@@ -5,13 +5,20 @@ package com.kamotelabs.com.facade;
  */
 
 public class Game {
-	
+
 	private InputSystem input = new InputSystem();
 	private GameObjects objects = new GameObjects();
 	private GameConsole screen = new GameConsole();
-	
+
 	public void update() {
-		
+		// Input
+		input.getInput();
+
+		// Update game objects (player, bad guys, etc)
+		objects.update(input);
+
+		// Draw
+		screen.draw(objects);
 	}
 
 }
